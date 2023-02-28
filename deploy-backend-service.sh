45c326e2c05661e8839f754d2421501032ec41a9
Nguyễn Văn Thương <nvthuong1508@gmail.com>
2023-03-01 00:02:56 +0700

    update docker compose

--- deploy-backend-service.sh
+++ deploy-backend-service.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
-docker-compose -f docker-compose-backend-service.yml up -d --force-recreate
+docker-compose -f docker-compose-backend-service.yml down
+
+#uncomment when new image built to remove old image in local
+# docker rmi -f thuongnguyen158/le-user-service:latest
 # docker rmi -f thuongnguyen158/le-file-service:latest
 # docker rmi -f thuongnguyen158/le-api-gateway:latest
+# docker rmi -f thuongnguyen158/le-web-app:latest
+#!/bin/bash
+docker-compose -f docker-compose-backend-service.yml up -d --force-recreate
 
-# docker-compose -f docker-compose-backend-service.yaml up -d --force-recreate
-# docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
