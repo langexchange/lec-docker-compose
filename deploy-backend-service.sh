001026a52b28f5c9931e732515d6ecd97010d94f
Nguyễn Văn Thương <nvthuong1508@gmail.com>
2023-03-20 01:17:34 +0700

    Update deploy-backend-service.sh

--- deploy-backend-service.sh
+++ deploy-backend-service.sh
@@ -1,7 +1,7 @@
 docker-compose -f docker-compose-backend-service.yml down
 
 #uncomment when new image built to remove old image in local
-# docker rmi -f thuongnguyen158/le-user-service:latest
+docker rmi -f thuongnguyen158/le-user-service:latest
 # docker rmi -f thuongnguyen158/le-file-service:latest
 # docker rmi -f thuongnguyen158/le-api-gateway:latest
 # docker rmi -f thuongnguyen158/le-web-app:latest
