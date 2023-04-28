2b8703cdcfec1c89fcf4d831ea9552398241c9e1
Nguyễn Văn Thương <nvthuong1508@gmail.com>
2023-04-29 01:39:35 +0700

    prepare deploy file

--- Deploy-files/deploy-consul.sh
+++ Deploy-files/deploy-consul.sh
@@ -1,1 +1,3 @@
+# docker network create lang-exchange-network
+# docker network create --attachable --driver bridge lang-exchange-network || true
 docker-compose -f docker-compose-consul.yaml up -d --force-recreate
--- Deploy-files/deploy-user-service.sh
+++ Deploy-files/deploy-user-service.sh
@@ -1,4 +1,4 @@
-docker-compose -f docker-compose-user-service.yaml down
+# docker-compose -f docker-compose-user-service.yaml down
 #remove old image
 # docker rmi -f thuongnguyen158/le-user-service:latest
 # docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
