8385cddb77b3ec57b3b6007eaec56563214adf8e
Nguyễn Văn Thương <nvthuong1508@gmail.com>
2023-06-02 15:24:43 +0700

    update

--- Deploy-files/deploy-api-gateway.sh
+++ Deploy-files/deploy-api-gateway.sh
@@ -1,6 +1,6 @@
 docker-compose -f docker-compose-api-gateway.yaml down
 #remove old image
-docker rmi -f thuongnguyen158/le-api-gateway:latest
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+# docker rmi -f thuongnguyen158/le-api-gateway:latest
+# docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 #run docker compose
 docker-compose -f docker-compose-api-gateway.yaml up -d --force-recreate
--- Deploy-files/deploy-user-service.sh
+++ Deploy-files/deploy-user-service.sh
@@ -1,5 +1,5 @@
 # docker-compose -f docker-compose-user-service.yaml down
-#remove old image
+# #remove old image
 # docker rmi -f thuongnguyen158/le-user-service:latest
 # docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 #run docker compose
